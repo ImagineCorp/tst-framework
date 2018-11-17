906e953b58c45c06742e204a19fcf62d5bca81ec
Dave R <davetheprogrammer2@gmail.com>
2018-11-17 19:06:47 +0000

    Update commandHandler.js

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -2,6 +2,8 @@ module.exports.run = (client,message,prefix) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     try {
-        require(`./commands/${cmd}.js`).run(client,message,args)
-    } catch (err) {}
+        require(`./commands/${cmd}.js`).run(client,message,args);
+    } catch (err) {
+        console.log(err);
+    }
 }
