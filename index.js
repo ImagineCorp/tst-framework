dc33f766558e4a6cb142f8028c7653f685cfae03
Dave R <davetheprogrammer2@gmail.com>
2018-11-17 19:12:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { Client }     = require('discord.js');
-const client         = new Client();
-const config         = require('./config.json');
-const commandHandler = require('./src/commandHandler.js');
+const { Client }     = require('discord.js')
+const client         = new Client()
+const config         = require('./config.json')
+const commandHandler = require('./src/commandHandler.js')
 
 client.on('ready', () => {
     console.log(`Hello world! ${client.guilds.size} servers`);
-    client.user.setPresence({game: {name: `stories | -help`, type: 'PLAYING'}});
+    client.user.setPresence({game: {name: `stories | -help`, type: 'PLAYING'}})
 });
 
 client.on('message', (message) => {
-    if(message.author.bot || !message.content.startsWith(prefix)) return;
+    if(message.author.bot || !message.content.startsWith(prefix)) return
 
-    commandHandler.run(client,message,prefix);
+    commandHandler.run(client,message,prefix)
 });
 
-client.login(config.token);
+client.login(config.token)
