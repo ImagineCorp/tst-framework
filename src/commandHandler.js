74e93564468ff60fc350591cabe10585c090021b
Dave R <davetheprogrammer2@gmail.com>
2018-11-17 19:12:40 +0000

    Update commandHandler.js

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -1,9 +1,7 @@
 module.exports.run = (client,message,prefix) => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const cmd = args.shift().toLowerCase()
     try {
-        require(`./commands/${cmd}.js`).run(client,message,args);
-    } catch (err) {
-        console.log(err);
-    }
+        require(`./commands/${cmd}.js`).run(client,message,args)
+    } catch (err) { }
 }
