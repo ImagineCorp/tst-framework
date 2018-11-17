c0213a0d7f943587c20a8f69016ff6603fa51762
Dave R <davetheprogrammer2@gmail.com>
2018-11-17 19:12:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const commandHandler = require('./src/commandHandler.js')
 client.on('ready', () => {
     console.log(`Hello world! ${client.guilds.size} servers`);
     client.user.setPresence({game: {name: `stories | -help`, type: 'PLAYING'}})
-});
+})
 
 client.on('message', (message) => {
     if(message.author.bot || !message.content.startsWith(prefix)) return
 
     commandHandler.run(client,message,prefix)
-});
+})
 
 client.login(config.token)
